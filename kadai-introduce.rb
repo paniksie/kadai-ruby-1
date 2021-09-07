4a9be58fa02ff86d563f4eceeb81a71b017f1698
kanta.kaneki <hotcola2488@gmail.com>
2021-09-07 08:10:03 +0000

    fixed

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -2,5 +2,4 @@ my_family_name = '四ツ目'
 my_first_name = '雅明'
 my_age = 23
 
-str = my_family_name + my_first_name + 'です。' + my_age.to_s + '歳です。'
-puts str
+puts my_family_name + my_first_name + 'です。' + my_age.to_s + '歳です。'
